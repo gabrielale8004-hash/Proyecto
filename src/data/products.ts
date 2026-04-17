22bc8d6d871f8a47c8349c937d108fb05e6539a9
gabriel teran <gabrielale8004@gmail.com>
2026-04-17 17:09:55 -0400

    cambie el apartado de productos

---
+++ src/data/products.ts
@@ -0,0 +1,20 @@
+export const listadoProductos = [
+    {
+        title: "Tornillo Hexagonal G5",
+        description:
+            "Tornillería de alta resistencia para uso industrial y automotriz.",
+        image: "/unnamed.png",
+    },
+    {
+        title: "Llave de Torque Prof.",
+        description:
+            "Herramienta de precisión con mango ergonómico y ajuste rápido.",
+        image: "/llave.jpeg",
+    },
+    {
+        title: "Bota de Seguridad",
+        description:
+            "Calzado con punta de acero y suela antideslizante certificada.",
+        image: "/bota.jpeg",
+    },
+];
